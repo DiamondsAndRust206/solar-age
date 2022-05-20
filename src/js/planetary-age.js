ee285f896e19e6291800bc366fa2bc866c8b353b
Timothy Gibson <ryaninlux@gmail.com>
2022-05-20 16:34:55 -0700

    Green phase PlanetaryAge class

--- src/js/planetary-age.js
+++ src/js/planetary-age.js
@@ -1,7 +1,7 @@
 export default class PlanetaryAge {
   constructor(age, expectancy) {
-    // this.age = age;
-    // this.expectancy = expectancy;
+    this.age = age;
+    this.expectancy = expectancy;
   }
   
   method() {
